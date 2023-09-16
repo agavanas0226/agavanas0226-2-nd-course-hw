b2e80253c08b99ed4b82e92b935b8be7f6846866
agavanas0226 <agavanas0226@gmail.com>
2023-09-16 14:27:29 +0600

    Исправление ошибок hw3

--- hw1/hw3.js
+++ hw1/hw3.js
@@ -30,16 +30,16 @@ console.log(Number(a) + Number(b));
 // Задание 5
     let monthNumber = prompt ('Введите номер месяца');
         switch (monthNumber) {
-        case '1': case '2': case '12': case 'первый': case 'Первый': case 'второй': case 'Второй': case 'двенадцатый': case 'Двенадцатый':
+        case '1': case '2': case '12':
             console.log('Зима');
             break;
-        case '3': case '4': case '5': case 'третий': case 'Третий': case 'четвертый': case 'Четвертый': case 'пятый': case 'Пятый':
+        case '3': case '4': case '5': 
             console.log ('Весна');
             break;
-        case '6': case '7': case '8': case 'шестой': case 'Шестой': case 'седьмой': case 'Седьмой': case 'восьмой': case 'Восьмой':
+        case '6': case '7': case '8': 
             console.log ('Лето');
             break;
-        case '9': case '10': case '11': case 'девятый': case 'Девятый': case 'десятый': case 'Десятый': case 'одиннадцатый': case 'Одиннадцатый':
+        case '9': case '10': case '11': 
             console.log ('Осень');
             break;
         default:
